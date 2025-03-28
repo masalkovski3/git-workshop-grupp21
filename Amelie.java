15b4428695a8119f16ae90a7febb23c8b26c8a34
Amelie Music <amelielisaa@gmail.com>
2025-03-28 10:25:31 +0100

    Food

--- Amelie.java
+++ Amelie.java
@@ -2,4 +2,8 @@ public class Amelie {
     public void amelie() {
         System.out.println("Amelie & Bamse <3");
     }
+
+    public void food() {
+        System.out.println("I am also food");
+    }
 }
